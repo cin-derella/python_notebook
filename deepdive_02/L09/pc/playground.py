192186bfea1387586db5c2dac0221b294c0725a5
cin-derella <shirley_cinderella@outlook.com>
2020-03-08 22:28:42 +0800

    project4D

--- deepdive_02/L09/pc/playground.py
+++ deepdive_02/L09/pc/playground.py
@@ -2,6 +2,7 @@ import constants
 import parse_utils
 import itertools
 from datetime import datetime
+from functools import partial
 
 # # see a sample of what is in each file
 # for fname in constants.fnames:
@@ -58,20 +59,106 @@ from datetime import datetime
 # print(nt._fields)
 
 
-data_iter = parse_utils.iter_combined(constants.fnames,
-                                      constants.class_names,
-                                      constants.parsers,
-                                      constants.compress_fields)
-for row in itertools.islice(data_iter,5):
+# data_iter = parse_utils.iter_combined(constants.fnames,
+#                                       constants.class_names,
+#                                       constants.parsers,
+#                                       constants.compress_fields)
+# for row in itertools.islice(data_iter, 5):
+#     print(row)
+#
+# print('-------------------')
+
+# cutoff_date = datetime(2018, 3, 1)
+#
+#
+# def group_key(item):
+#     return item.vehicle_make
+#
+#
+# data = parse_utils.filtered_iter_combined(constants.fnames,
+#                                           constants.class_names,
+#                                           constants.parsers,
+#                                           constants.compress_fields,
+#                                           key=lambda row: row.last_updated >= cutoff_date)
+# data_1,data_2 = itertools.tee(data,2)
+#
+# data_m = (row for row in data_1 if row.gender =='Male')
+# sorted_data_m = sorted(data_m,key=group_key)
+# groups_m = itertools.groupby(sorted_data_m,key=group_key)
+# group_m_counts = ((g[0],len(list(g[1]))) for g in groups_m)
+# print('group_m')
+# for row in group_m_counts:
+#     print(row)
+#
+# print()
+#
+# data_f = (row for row in data_2 if row.gender =='Female')
+# sorted_data_f = sorted(data_f,key=group_key)
+# groups_f = itertools.groupby(sorted_data_f,key=group_key)
+# group_f_counts = ((g[0],len(list(g[1]))) for g in groups_f)
+# print('group_f')
+# for row in group_f_counts:
+#     print(row)
+
+
+# sorted_data = sorted(data, key=group_key)
+#
+# groups_1 = itertools.groupby(sorted_data,key=group_key)
+#
+# groups_2 = itertools.groupby(sorted_data,key=group_key)
+# #
+# # rg1 = next(groups_1)
+# # rg2 = next(groups_2)
+# # print(rg1[0],id(rg1[1]))
+# # print(rg2[0],id(rg2[1]))
+# #
+# # print()
+# #
+# # rg1 = next(groups_1)
+# # rg2 = next(groups_2)
+# # print(rg1[0],id(rg1[1]))
+# # print(rg2[0],id(rg2[1]))
+#
+#
+# group_f = (item for item in groups_1 if item[0][0]=='Female')
+# data_f = ((item[0][1],len(list(item[1]))) for item in group_f)
+# print('group_f')
+# for row in data_f:
+#     print(row)
+#
+#
+# group_m = (item for item in groups_2 if item[0][0]=='Male')
+#
+# data_m = ((item[0][1],len(list(item[1]))) for item in group_m)
+# print('group_m')
+# for row in data_m:
+#     print(row)
+
+cutoff_date = datetime(2018, 3, 1)
+
+def filter_key(cutoff_date, gender,row):
+    return row.last_updated>=cutoff_date and row.gender == gender
+
+
+results_f = parse_utils.group_data(constants.fnames,
+                                   constants.class_names,
+                                   constants.parsers,
+                                   constants.compress_fields,
+                                   filter_key= partial(filter_key,cutoff_date,'Famale'),
+                                   group_key=lambda row: row.vehicle_make)
+
+results_m = parse_utils.group_data(constants.fnames,
+                                   constants.class_names,
+                                   constants.parsers,
+                                   constants.compress_fields,
+                                   filter_key= lambda row :filter_key(cutoff_date,'Male',row),
+                                   group_key=lambda row: row.vehicle_make)
+
+print('results_f')
+for row in results_f:
     print(row)
+print()
 
-print('-------------------')
-
-cutoff_date = datetime(2018,3,1)
-filtered_iter = parse_utils.filtered_iter_combined(constants.fnames,
-                                      constants.class_names,
-                                      constants.parsers,
-                                      constants.compress_fields,
-                                      key = lambda row : row.last_updated >= cutoff_date)
-for row in filtered_iter:
+print('results_m')
+for row in results_m:
     print(row)
